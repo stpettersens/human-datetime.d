4aa49adaa68d03048e19a8f6dd3827a86e95377b
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2025-11-11 15:55:53 +0000

    ...

--- human_datetime_demo.d
+++ human_datetime_demo.d
@@ -2,7 +2,7 @@ import std.stdio;
 import human_datetime;
 
 int main() {
-    writeln(convert_datetime_to_timezone("11 Nov 2025 15:45:00 GMT", "EST"));
+    writeln(convert_datetime_to_timezone("11 Nov 2025 15:52:00 GMT", "NPT"));
     writeln(human_to_unix("11 Nov 2025 14:58:00 GMT"));
     writeln();
     writeln(unix_now());
